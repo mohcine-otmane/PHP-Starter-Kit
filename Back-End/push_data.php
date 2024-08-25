a2167ad74da2216e0b83066a16d5a5971768da3b
mohcine-otmane <littlesandflea@gmail.com>
2024-08-25 20:32:47 +0100

    Tested

--- Back-End/push_data.php
+++ Back-End/push_data.php
@@ -7,12 +7,16 @@ include "database.php";
 
 
 // Create users table
+
+// Put the SQL query in a variable $sql
 $sql = "CREATE TABLE users (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(20) NOT NULL,
 email VARCHAR(50)
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
+
+// $conn->query($sql) executes the query
 if ($conn->query($sql) === TRUE) {
     echo "Table created";
 } else {
@@ -21,7 +25,6 @@ if ($conn->query($sql) === TRUE) {
 
 
 // Insert data in users
-
 $sql = "INSERT INTO users (username, email)
 VALUES ('Mohcine', 'mohcine@yahoo.com')
 ";
@@ -34,5 +37,5 @@ if ($conn->query($sql) === TRUE) {
 }
 
 
-
+// Close the connection
 $conn->close();
